60ff557716cb98cd609475461425f635e7eac98e
Emna_Cheniour <emnacheniour2000@gmail.com>
2023-11-30 17:27:12 +0100

    monitoring

--- hello.js
+++ hello.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const prometheus = require('prom-client');
+const collectDefaultMetrics = prometheus.collectDefaultMetrics;
+collectDefaultMetrics({ timeout: 5000}); // on collecte les métriques par défaut chaque 5sec
+
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', prometheus.register.contentType);
+  res.end(prometheus.register.metrics());
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
